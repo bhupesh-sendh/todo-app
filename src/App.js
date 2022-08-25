37604898f75b3232b2e053bfcf170f3403230303
bhupesh-sendh <bhupeshsendh@gmail.com>
2022-08-25 14:28:47 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -8,36 +8,67 @@ import Body from './Components/LearningComponents/Body'
 import Button from 'react-bootstrap/Button';
 import {useState} from 'react'
 
-function App(){
-  const[count,setcount]=useState(0)
-  const[user,setUser]=useState("")
-  const onChange=e=>{ //e stands for each and every event
-    setUser(e.target.value)
-  }
-  const arr=["html","css","JS","ReactJS"]
-  // Array.map(    //Syntax of array.map function
-  //   (value,index)=><li>{value}</li>
-  // )
 
- 
-  useEffect(()=>console.log(count),[count])// after exeduting the below return only useEffect will work
+function App(){
+  const[input,setInput]=useState("")
+  const[result,setResult]=useState(0)
+  const handler=e=>{
+    setInput(e.target.value)
+  }
+
   return(
     <div className='App'>
-      <h1>{count}</h1>
-      <button onClick={()=>setcount(count+1)}>Click here</button>
-      <input type="text" value={user} name="user" placeholder='Username' onChange={onChange} />
-      <br></br>
-      {user}
-      {
-      arr.map(
-        (value,index)=><li>{value}</li>
-      )
-      }  {/* //since its js code therefore have to enclose within {} */}
-      
-      
+      <input type="text" placeholder={input} value={input} name="input" onChange={handler} />
+      <br /> 
+      <button onClick={()=>setResult(eval(input))}>Result</button>
+      {/* <button onClick={()=>setInput(input="")}>Reset</button> */}
+      <h1>Result is:{result}</h1>
+      <Button onClick={()=>setInput(input+'1')}>1</Button>
+      <Button onClick={()=>setInput(input+'2')}>2</Button>
+      <Button onClick={()=>setInput(input+'3')}>3</Button>
+      <br />
+      <Button onClick={()=>setInput(input+'4')}>4</Button>
+      <Button onClick={()=>setInput(input+'5')}>5</Button>
+      <Button onClick={()=>setInput(input+'6')}>6</Button>
+      <br />
+      <Button onClick={()=>setInput(input+'7')}>7</Button>
+      <Button onClick={()=>setInput(input+'8')}>8</Button>
+      <Button onClick={()=>setInput(input+'9')}>9</Button>
+
     </div>
   )
 }
+
+// function App(){
+//   const[count,setcount]=useState(0)
+//   const[user,setUser]=useState("")
+//   const onChange=e=>{ //e stands for each and every event
+//     setUser(e.target.value)
+//   }
+//   const arr=["html","css","JS","ReactJS"]
+//   // Array.map(    //Syntax of array.map function
+//   //   (value,index)=><li>{value}</li>
+//   // )
+
+ 
+//   useEffect(()=>console.log(count),[count])// after exeduting the below return only useEffect will work
+//   return(
+//     <div className='App'>
+//       <h1>{count}</h1>
+//       <button onClick={()=>setcount(count+1)}>Click here</button>
+//       <input type="text" value={user} name="user" placeholder='Username' onChange={onChange} />
+//       <br></br>
+//       {user}
+//       {
+//       arr.map(
+//         (value,index)=><li>{value}</li>
+//       )
+//       }  {/* //since its js code therefore have to enclose within {} */}
+      
+      
+//     </div>
+//   )
+// }
 
 
 // class App extends Component{
